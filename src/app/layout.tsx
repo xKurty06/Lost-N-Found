fb528da7c2f122f40e7f96a028ad02b2cd2456b8
xKurty06 <zeankurtbalboa06@gmail.com>
2025-05-25 19:14:44 +0800

    feat: fixed login page removed navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import "./globals.css";
+"use client";
+import { usePathname } from 'next/navigation';
 import Navbar from "./navbar";
-
-export const metadata: Metadata = {
-    title: "Lost-N-Found",
-    description: "",
-};
+import "./globals.css";
 
 export default function RootLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
+    const pathname = usePathname();
+    const hideNavbar = pathname.startsWith('/login');
     return (
         <html lang="en">
             <body className="bg-white">
-                <div className="fixed top-0 left-0 w-full z-40">
-                    <Navbar />
-                </div>
+                {!hideNavbar && (
+                    <div className="fixed top-0 left-0 w-full z-40">
+                        <Navbar />
+                    </div>
+                )}
                 <div className="">
                     <main>
                         {children}
